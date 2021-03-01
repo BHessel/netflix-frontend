ec122cefc30500deb394d64890ba98c56bfb2817
BHessel <hesselfitness@gmail.com>
2021-03-01 16:40:08 -0500

    4:40

--- src/account.js
+++ src/account.js
@@ -8,7 +8,14 @@ export default class account extends Component {
         <>
             <div className="userSearch">
                 <h2>Find a User</h2>
+                <div className="relative pt-2 mx-auto text-gray-600">
+                    <input className="h-10 px-5 pr-16 text-sm bg-white border-2 border-gray-300 rounded-lg focus:outline-none" type="search" name="search" placeholder="Search">
+                    </input>
+                    <button type="submit" className="absolute top-0 right-0 mt-5 mr-4">
+                    </button>
+                </div>
             </div>
+            
             <div className="currentMatches">
                 <h2>Current Matches</h2>
             </div>
--- src/showMatches.js
+++ src/showMatches.js
@@ -3,9 +3,10 @@ import React, { Component } from 'react'
 export default class showMatches extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="matchList">
+                <h1>See your matches with (other user)</h1>
             </div>
+        
         )
     }
 }
