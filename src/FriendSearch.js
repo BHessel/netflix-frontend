26303d60dcc48f07b9e59a53a34c05c55d2563c4
BHessel <hesselfitness@gmail.com>
2021-03-04 15:50:56 -0500

    me+becca partnership created

--- src/FriendSearch.js
+++ src/FriendSearch.js
@@ -1,6 +1,35 @@
 import React, { Component } from 'react'
 
 export default class FriendSearch extends Component {
+
+    state = {
+        partnership: false,
+        partner1_id: 1,
+        partner2_id: ""
+    }
+
+    createPartnership = (e) => {
+        e.preventDefault();
+        
+        let partnership = {
+            partner1_id: 1,
+            partner2_id: this.props.location.state.searchedUser[0].id,
+            partner1_approval: "",
+            partner2_approval: "",
+            trailer_id: ""
+        }
+
+        let requestPackage = {
+        method: "POST",
+        headers: {"Content-Type": "application/json", Accept: "application/json"},
+        body: JSON.stringify({ partnership })
+        }
+        
+        fetch('http://localhost:3000/partnerships', requestPackage)
+            .then((r) => r.json())
+            .then((resp) => console.log(resp))
+    }
+    
     render() {
         return (
             <>
@@ -12,7 +41,7 @@ export default class FriendSearch extends Component {
                     <div className="searchedUserName">
                       <p>{this.props.location.state.searchedUser[0].username}</p>
                     </div>
-                    <button>Add to Friend List</button> 
+                    <button type="submit" onClick={this.createPartnership}>Add to Friend List</button> 
                 </div>
                 
                 <div className="return-btn">
@@ -22,16 +51,3 @@ export default class FriendSearch extends Component {
         )
     }
 }
-
-
-//state needs to know
-//createPartnership = () => {
-//let partnership = {
-//partner1_id: (need access to currentUser's ID here), partner2_id: {this.props.location.state.searchedUser[0].id}    
-//}
-// fetch (local/parnterships, { method: "POST", headers: {}, body{})
-//.then(r=>r)
-//.then set state pf created
-//
-//    
-//}
